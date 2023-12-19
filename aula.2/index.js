180b429d62297e5947bff0ecf1a06303cfaea265
Luihza Marques <marquesluihza@gmail>
2023-12-18 20:35:12 -0400

    node-aula-2-crud

--- aula.2/index.js
+++ aula.2/index.js
@@ -1,6 +1,65 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
 let users = [
     
         {id: 1, name:"Euclides"},
         {id: 2, name:"Felipe"},
-        {id: 3, name:"Maria"}, 
+        {id: 3, name:"Maria"}
 ]
+
+app.get("/", function(req,res){
+    res.send(users)
+})
+
+
+app.get('/:id', function(req,res){
+    const id = parseInt(req.params.id)
+    const user= users.find(user => user.id === id)
+    if(user){
+        res.json(user)
+    }
+    else {
+        res.status(400).json({message: "Usuario não encontrado"})
+    }
+})
+
+
+app.post("/users", function(req,res){
+    console.log(req.body);
+    const { name } = req.body
+    const id = users.length+1
+    const newUser = {id,name}
+
+    users.push(newUser)
+    res.status(201).json(newUser)
+})
+
+app.delete("/users/:id",(req,res)=> {
+
+    const id = parseInt(req.params.id)
+    users = users.filter(user => user.id !== id);
+    res.sendStatus(204) 
+})
+
+app.put('users/:id', (req,res) =>{
+
+    const id = parseInt(req,params.id)
+
+    const {name} = req.body
+    userIndex = users.findIndex(user => user.id === id);
+
+    if(userIndex !== -1){
+        users[userIndex].name = name; 
+        res.json(users[userIndex])
+    } else {
+        res.status(400).json({message: "usuario nao encontrado"})
+    }
+})
+
+
+
+app.listen(3000)
